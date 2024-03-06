d60b83986b4347b17e4c73db6ddff76539423726
nirmala21549 <nirmala21549@gmail.com>
2024-03-07 05:20:17 +0530

    refactored

---
+++ check_palindrome.py
@@ -0,0 +1,8 @@
+def check_palindrome(text):
+   if text == text[::-1]:
+      print(f"{text} is a palindrome")
+   else:
+      print(f"{text} is not a palindrome")
+
+text = "did"
+check_palindrome(text)
