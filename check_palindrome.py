1b2e433e2547a672b87b665d6430ec4cb1c75dad
nirmala21549 <nirmala21549@gmail.com>
2024-03-07 16:04:22 +0530

    Resolve merge conflicts in check_palindrome.py

--- check_palindrome.py
+++ check_palindrome.py
@@ -1,14 +1,9 @@
 def check_palindrome(text):
-   if text == text[::-1]:
-      print(f"{text} is a palindrome")
-   else:
-      print(f"{text} is not a palindrome")
+    if text == text[::-1]:
+        print(f"{text} is a palindrome")
+    else:
+        print(f"{text} is not a palindrome")
+
 
 text = input("Enter text to check for palindrome: ")
 check_palindrome(text)
-try:
-   file = input("Enter filepath for palindrome check: ")
-   text = open(file, "r").read()
-   check_palindrome(text)
-except(OSError):
-   print(f"Unable to process file at {file}")
