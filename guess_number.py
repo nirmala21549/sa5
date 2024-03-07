1f85e76dfd9a11f58362fd2d64f0150c74fab029
nirmala21549 <nirmala21549@gmail.com>
2024-03-07 10:28:23 +0530

    Allow continuous guessing and option to quit

---
+++ guess_number.py
@@ -0,0 +1,15 @@
+number = 10
+
+print("I'm thinking of a number...")
+
+while True:
+    guess = input("What number am I thinking of? (Enter 'q' to quit) ")
+
+    if guess.lower() == 'q':
+        print(f"The number was {number}.")
+        break
+    elif int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print("Sorry! That's not the correct number. Try again.")
